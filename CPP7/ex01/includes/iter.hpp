d80d789c13587946c38c72b8dab42a61c868cdfa
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-05-30 13:27:40 +0100

    ex01 done

---
+++ CPP7/ex01/includes/iter.hpp
@@ -0,0 +1,19 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T, typename Func>
+void iter(T *array, int len, Func function) {
+	for (int i = 0; i < len; i++)
+		function(array[i]);
+}
+
+//for testing
+template <typename T>
+void print(T *array, int size) {
+	for (int i = 0; i < size; i++)
+		std::cout << array[i] << ", ";
+}
+
+#endif
---
+++ CPP7/ex01/sources/main.cpp
@@ -0,0 +1,32 @@
+#include "../includes/iter.hpp"
+
+void makeIt42(int &n) {
+	n = 42;
+}
+
+void iLoveMinishell(std::string &s) {
+	s = "ILOVEMINISHELL";
+}
+
+int main(void)
+{
+	std::string sentences[] = {"I", "HATE", "MINISHELL"};
+	int numbers[] = {1, 2, -1, 150};
+
+	std::cout << "Printing Sentences" << std::endl;
+	::print(sentences, 3);
+	std::cout << std::endl;
+	std::cout << "Printing Numbers" << std::endl;
+	::print(numbers, 4);
+	std::cout << std::endl;
+	std::cout << "Making Number into 42" << std::endl;
+	::iter(numbers, 4, makeIt42);
+	std::cout << "Printing Numbers" << std::endl;
+	::print(numbers, 4);
+	std::cout << std::endl;
+	std::cout << "Turning Hate into Love" << std::endl;
+	::iter(sentences, 3, iLoveMinishell);
+	std::cout << "Printing Sentences" << std::endl;
+	::print(sentences, 3);
+	std::cout << std::endl;
+}
