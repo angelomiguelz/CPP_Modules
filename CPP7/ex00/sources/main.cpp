9ac3336dd74b07d1a2828bc904a5a1196c48ac28
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-05-30 13:04:48 +0100

    ex00 done

--- CPP7/ex00/sources/main.cpp
+++ CPP7/ex00/sources/main.cpp
@@ -1,10 +1,18 @@
-#include "../includes/Scalar.hpp"
+#include "../includes/whatever.hpp"
 
-int main(int ac, char **av)
+int main(void)
 {
-	if (ac != 2) {
-		std::cout << "invalid input" << std::endl;
-		return 1;
-	}
-	Scalar::convert(av[1]);
+	int a = 2;
+	int b = 3;
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+	return 0;
 }
